f613293f7e2ac7b8bf5dc045d69d413a10408b93
rjacomel <ricardo@sekur.com.br>
2023-09-28 07:47:23 -0300

    Desafio OrderRepository

---
+++ desafios/07-ddd/src/domain/entity/customer.spec.ts
@@ -0,0 +1,65 @@
+import Address from "./address";
+import Customer from "./customer";
+
+describe("Customer unit tests", () => {
+  it("should throw error when ID is empty", () => {
+    expect(() => {
+      let customer = new Customer("", "Ricardo");
+    }).toThrowError("ID is required");
+  });
+
+  it("should throw error when name is empty", () => {
+    expect(() => {
+      let customer = new Customer("123", "");
+    }).toThrowError("Name is required");
+  });
+
+  it("should change name", () => {
+    // Triple A (Arrange, Act, Assert)
+    // Arrange
+
+    const customer = new Customer("123", "John");
+
+    // Act
+    customer.changeName("Ricardo");
+
+    // Assert
+    expect(customer.name).toBe("Ricardo");
+  });
+
+  it("should activate customer", () => {
+    const customer = new Customer("1", "Cusotmer 1");
+    const address = new Address("Street 1", 123, "12345-678", "Araucária");
+    customer.Address = address;
+
+    customer.activate();
+
+    expect(customer.isActive()).toBe(true);
+  });
+
+  it("should deactivate customer", () => {
+    const customer = new Customer("1", "Cusotmer 1");
+
+    customer.deactivate();
+
+    expect(customer.isActive()).toBe(false);
+  });
+
+  it("should throw error when address is undefined when you activate a customer", () => {
+    expect(() => {
+      const customer = new Customer("1", "Customer 1");
+      customer.activate();
+    }).toThrowError("Address is mandatory to activate a customer");
+  });
+
+  it("should add reward points", () => {
+    const customer = new Customer("1", "Customer 1");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(20);
+  });
+});
