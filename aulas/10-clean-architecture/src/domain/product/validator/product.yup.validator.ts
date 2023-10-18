14c0e8adf1e7123ee74b1253b2d24ae03f53c4f5
rjacomel <ricardo@sekur.com.br>
2023-10-18 09:01:08 -0300

    Desafio Validacao Products

---
+++ aulas/10-clean-architecture/src/domain/product/validator/product.yup.validator.ts
@@ -0,0 +1,44 @@
+import * as yup from "yup";
+import ValidatorInterface from "../../@shared/validator/validator.interface";
+import Product from "../entity/product";
+
+export default class ProductYupValidator
+  implements ValidatorInterface<Product>
+{
+  validate(entity: Product): void {
+    try {
+      yup
+        .object()
+        .shape({
+          id: yup.string().required("Id is required"),
+          name: yup.string().required("Name is required"),
+          price: yup
+            .number()
+            .test(
+              "Is positive?",
+              "Price must be greater than zero",
+              (value) => value > 0
+            )
+            .required("Price must be greater than zero"),
+        })
+        .validateSync(
+          {
+            id: entity.id,
+            name: entity.name,
+            price: entity.price,
+          },
+          {
+            abortEarly: false,
+          }
+        );
+    } catch (errors) {
+      const e = errors as yup.ValidationError;
+      e.errors.forEach((error) => {
+        entity.notification.addError({
+          context: "product",
+          message: error,
+        });
+      });
+    }
+  }
+}
