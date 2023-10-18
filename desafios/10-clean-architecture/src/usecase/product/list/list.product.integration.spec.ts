ae48ced14fcddcc0881af53b577c198496fb221c
rjacomel <ricardo@sekur.com.br>
2023-10-18 01:15:16 -0300

    Desafio Usecase para Product

---
+++ desafios/10-clean-architecture/src/usecase/product/list/list.product.integration.spec.ts
@@ -0,0 +1,58 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../../../infrastructure/product/repository/sequilize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequilize/product.repository";
+import ListProductUseCase from "./list.product.usecase";
+import Product from "../../../domain/product/entity/product";
+
+describe("test list product use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should list a product", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new ListProductUseCase(productRepository);
+
+    const product1 = new Product("123", "Product 1", 100);
+    const product2 = new Product("456", "Product 2", 250);
+
+    await productRepository.create(product1);
+    await productRepository.create(product2);
+
+    const output1 = {
+      id: "123",
+      name: "Product 1",
+      price: 100,
+    };
+
+    const output2 = {
+      id: "456",
+      name: "Product 2",
+      price: 250,
+    };
+
+    const output = await usecase.execute({});
+
+    expect(output.products.length).toBe(2);
+    expect(output.products[0].id).toBe(product1.id);
+    expect(output.products[0].name).toBe(product1.name);
+    expect(output.products[0].price).toBe(product1.price);
+    expect(output.products[1].id).toBe(product2.id);
+    expect(output.products[1].name).toBe(product2.name);
+    expect(output.products[1].price).toBe(product2.price);
+  });
+});
